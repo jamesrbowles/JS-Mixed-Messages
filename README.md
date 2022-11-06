ba0b3f785e18e705b521b4ea94fe47a0a5bfc3da
James Bowles <jamesrbowles@gmail.com>
2022-11-06 16:04:47 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#Muhammad Ali random quote generator#
+
+Every time a user runs this program they will get a random quote said by Muhammad Ali
+
+##Purpose##
+
+This is a portfolio project from Codecademy, testing Javascript, command line, git and github knowledge.
+
+##Languages##
+
+* Javascript
---
+++ app.js
@@ -0,0 +1,8 @@
+// Muhammad Ali random quote generator
+
+// create an array of quotes
+
+let quotes = ["Float like a butterfly, sting like a bee.", "Don't count the days. Make the days count.", "Impossible is temporary. Impossible is nothing.", "If my mind can conceive it, if my heart can believe it--then I can achieve it.", "I am the greatest. I said that even before I knew I was.", "A man who has no imagination has no wings.", "It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe.", "It's not bragging if you can back it up.", "I should be a postage stamp. That's the only way I'll ever get licked.", "He who is not courageous enough to take risks will accomplish nothing in life.", "I shook up the world. Me!"]
+
+
+//create a function that returns a random quote
